9a850f7a7e7ccaf5ee7841e60d9bc7b8b079227b
Tsolmon Togtokhsaikhan <23LP9801@G301-CC0180.local>
2023-11-16 13:06:55 +0800

    d1

---
+++ src/components/Intro.js
@@ -0,0 +1,18 @@
+export const Intro = () => {
+  return (
+    <div className="w-[100%] flex-row justify-center p-[10px]">
+      <div className="flex-col gap-[50px]">
+        <div className="flex-col gap-[20px]">
+          <p className="text-3xl font-bold">Hi, I’m Chomo 👋</p>
+          <p className="w-[45%]">
+            I'm a full stack developer (React.js & Node.js) with a focus on
+            creating (and occasionally designing) exceptional digital
+            experiences that are fast, accessible, visually appealing, and
+            responsive. Even though I have been creating web applications for
+            over 7 years, I still love it as if it was something new.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+export const Nav = () => {
+  return (
+    <div className="w-[80%] mt-[50px] flex justify-between">
+      <div className="felx-row">
+        <h3 className="text-3xl font-bold">&#60;SS&#47;&#62; </h3>
+      </div>
+      <div className="flex gap-x-10 text-base justify-center items-center">
+        <p>About</p>
+        <p>Work</p>
+        <p>Testimonials</p>
+        <p>Contact</p>
+        <img src="/Divider.svg" />
+        <img src="/Icon Button.svg" />
+        <p className="p-[5px] bg-[#111827] rounded-lg text-white">Download</p>
+      </div>
+    </div>
+  );
+};
